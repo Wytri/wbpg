c74139e72d9a1d53b92ce05ea28f4bb0840cc03a
Jefferson1307 <jefrirochambrun@gmail.com>
2023-04-20 22:36:08 -0500

    Se sube

--- Proyecto_WEB/src/main/java/SQL/SQLConexion.java
+++ Proyecto_WEB/src/main/java/SQL/SQLConexion.java
@@ -6,11 +6,11 @@ public static Connection getConexion(){
 Connection con = null;
  try { Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); 
 String url ="jdbc:sqlserver://localhost;database=cine"; 
-String usr = "userSQL"; 
-String psw = "260403"; 
+String usr = "sa"; 
+String psw = "1234"; 
 con = DriverManager.getConnection(url,usr,psw); } 
 catch (ClassNotFoundException ex) { System.out.println("No hay Driver!!"); } 
 catch (SQLException ex) 
 { System.out.println("Error con la BD"); }
  return con; }
- } 
+ }
